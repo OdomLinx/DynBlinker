5c2bd5bbe4e6490c2bdb8269a9af9a52f4083c44
OdomLinx <c.schulz@rabe11.de>
2017-04-22 00:10:36 +0200

    formating code

--- dynblinker.cpp
+++ dynblinker.cpp
@@ -1,6 +1,6 @@
 #include "dynblinker.h"
 
-dynblinker::dynblinker (uint8_t pin, uint16_t NUM_PIXELS) {
+dynblinker::dynblinker(uint8_t pin, uint16_t NUM_PIXELS) {
   this->pin = pin;
   this->NUM_PIXELS = NUM_PIXELS;
   this->startpixel = 0;
@@ -21,23 +21,21 @@ dynblinker::dynblinker (uint8_t pin, uint16_t NUM_PIXELS) {
   this->blinkddelayed = false;
 }
 
-dynblinker::dynblinker (uint8_t pin, uint16_t startpixel, uint16_t endpixel) {
+dynblinker::dynblinker(uint8_t pin, uint16_t startpixel, uint16_t endpixel) {
   this->pin = pin;
 
   this->startpixel = startpixel;
   this->endpixel = endpixel;
-  if (this->startpixel > this->endpixel)
-  {
-    this->NUM_PIXELS = this->startpixel-this->endpixel;
+  if (this->startpixel > this->endpixel) {
+    this->NUM_PIXELS = this->startpixel - this->endpixel;
     this->offset = this->endpixel;
-  }
-  else
-    {
-    this->NUM_PIXELS = this->endpixel-this->startpixel;
+  } else {
+    this->NUM_PIXELS = this->endpixel - this->startpixel;
     this->offset = this->startpixel;
-    }
+  }
 
-  this->strip = Adafruit_NeoPixel(this->NUM_PIXELS+this->offset, pin, NEO_GRB + NEO_KHZ800);
+  this->strip = Adafruit_NeoPixel(this->NUM_PIXELS + this->offset, pin,
+                                  NEO_GRB + NEO_KHZ800);
   this->color = this->strip.Color(255, 69, 0);
   this->blinkonrun = false;
   this->blinkonready = false;
@@ -52,12 +50,11 @@ dynblinker::dynblinker (uint8_t pin, uint16_t startpixel, uint16_t endpixel) {
   this->blinkddelayed = false;
 }
 
-
 void dynblinker::begin() {
   this->strip.begin();
   this->strip.show(); // Initialize all pixels to 'off'
 }
-void dynblinker::setPixel(uint16_t pixel, uint32_t color)	{
+void dynblinker::setPixel(uint16_t pixel, uint32_t color) {
   Serial.print("setpixel: ");
   Serial.println(pixel);
   Serial.print("color: ");
@@ -67,99 +64,76 @@ void dynblinker::setPixel(uint16_t pixel, uint32_t color)	{
 }
 void dynblinker::blinkon() {
 
-   if (this->blinkoncount < this->NUM_PIXELS)
-  {
+  if (this->blinkoncount < this->NUM_PIXELS) {
 
     this->blinkonrun = true;
     unsigned long currentMillis = millis();
     if (currentMillis - this->blinkpreviousMillis >= this->blinkinterval) {
-      uint16_t pixel=0;
-      if (this->startpixel > this->endpixel)
-      {
-      pixel= this->NUM_PIXELS -  this->blinkoncount;
-      }
-      else
-      {
-        pixel= this->blinkoncount;
+      uint16_t pixel = 0;
+      if (this->startpixel > this->endpixel) {
+        pixel = this->NUM_PIXELS - this->blinkoncount;
+      } else {
+        pixel = this->blinkoncount;
       }
       pixel = pixel + this->offset;
-       setPixel(pixel, this->color);
+      setPixel(pixel, this->color);
       this->blinkoncount++;
       this->blinkpreviousMillis = currentMillis;
     }
-  }
-  else
-  {
-   this->strip.clear();
+  } else {
+    this->strip.clear();
     this->blinkonready = true;
     this->blinkddelayed = false;
   }
-
-
-
-
 }
 
 void dynblinker::blinkoff() {
-   if (this->blinkoffcount < this->NUM_PIXELS)
-  {
+  if (this->blinkoffcount < this->NUM_PIXELS) {
     Serial.println("off");
     this->blinkoffrun = true;
-    uint16_t pixel=0;
+    uint16_t pixel = 0;
     pixel = this->blinkoffcount + this->offset;
     setPixel(pixel, 0);
     this->blinkoffcount++;
 
-  }
-  else
-  {
+  } else {
     this->strip.clear();
     this->blinkoffready = true;
     this->blinkddelayed = false;
   }
-
 }
 void dynblinker::blink() {
 
-  if (this->blinkoffrun == false)
-  {
+  if (this->blinkoffrun == false) {
 
     blinkon();
-  }
-  else
-  {
+  } else {
 
-  if (this->blinkonready)
-    {
+    if (this->blinkonready) {
 
-    blinkoff();
+      blinkoff();
     }
   }
 
   if (this->blinkddelayed == false) {
 
     unsigned long currentMillis = millis();
-      if (currentMillis - this->blinkdpreviousMillis >= this->blinkdinterval) {
+    if (currentMillis - this->blinkdpreviousMillis >= this->blinkdinterval) {
 
-        if (blinkonready== true)
-          {
+      if (blinkonready == true) {
 
         this->blinkonrun = false;
         this->blinkoffrun = true;
         this->blinkoncount = 0;
       }
- if (blinkoffready==true)
-  {
-      this->blinkoffrun = false;
-      this->blinkoffcount = 0;
-      this->blinkoffready = false;
-
-    }
+      if (blinkoffready == true) {
+        this->blinkoffrun = false;
+        this->blinkoffcount = 0;
+        this->blinkoffready = false;
+      }
 
       this->blinkddelayed = true;
       this->blinkdpreviousMillis = currentMillis;
     }
-
-
   }
 }
--- dynblinker.h
+++ dynblinker.h
@@ -1,52 +1,46 @@
 #ifndef __DYN_Blinker__
 #define __DYN_Blinker__
 
-#include <Arduino.h>
 #include <Adafruit_NeoPixel.h>
-
-
+#include <Arduino.h>
 
 class dynblinker {
 
-	private:
-		uint8_t pin;
-		uint16_t NUM_PIXELS;
-		uint16_t startpixel;
-		uint16_t endpixel;
-		uint16_t offset;
-		uint32_t color;
-		Adafruit_NeoPixel strip;
-		bool blinkonrun;
-		bool blinkonready;
-		uint16_t blinkoncount;
-		bool blinkoffrun;
-		bool blinkoffready;
-		uint16_t blinkoffcount;
-		unsigned long blinkpreviousMillis;
-		uint16_t blinkinterval;
-    unsigned long blinkdpreviousMillis;
-    uint16_t blinkdinterval;
-    bool blinkddelayed;
+private:
+  uint8_t pin;
+  uint16_t NUM_PIXELS;
+  uint16_t startpixel;
+  uint16_t endpixel;
+  uint16_t offset;
+  uint32_t color;
+  Adafruit_NeoPixel strip;
+  bool blinkonrun;
+  bool blinkonready;
+  uint16_t blinkoncount;
+  bool blinkoffrun;
+  bool blinkoffready;
+  uint16_t blinkoffcount;
+  unsigned long blinkpreviousMillis;
+  uint16_t blinkinterval;
+  unsigned long blinkdpreviousMillis;
+  uint16_t blinkdinterval;
+  bool blinkddelayed;
 
-	public:
-		dynblinker (uint8_t pin, uint16_t NUM_PIXELS);  //Default initial, use all pixel
-		dynblinker (uint8_t pin, uint16_t startpixel, uint16_t endpixel);  //use only defined pixel
+public:
+  dynblinker(uint8_t pin, uint16_t NUM_PIXELS); // Default initial, use all
+                                                // pixel
+  dynblinker(uint8_t pin, uint16_t startpixel,
+             uint16_t endpixel); // use only defined pixel
 
-		void begin();
-		void setPixel(uint16_t pixel, uint32_t color);
-		void blink();
-		void blinkon();
-		void blinkoff();
-		void clear();
+  void begin();
+  void setPixel(uint16_t pixel, uint32_t color);
+  void blink();
+  void blinkon();
+  void blinkoff();
+  void clear();
 
-
-		//void setRGB(int r, int g, int b) ;
-		//void blink(char color, byte times, unsigned int ms);
+  // void setRGB(int r, int g, int b) ;
+  // void blink(char color, byte times, unsigned int ms);
 };
-
-
-
-
-
 
 #endif
